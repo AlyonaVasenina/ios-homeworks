6ecbcdb80853ff4c7a12ea47f7e226ef6e0e38fa
Alyona Vasenina <alyona.vasenina@gmail.com>
2021-10-29 01:45:17 +0300

    Add alertButton in InfoVC

--- Navigation/Navigation/InfoVC.swift
+++ Navigation/Navigation/InfoVC.swift
@@ -12,5 +12,18 @@ class InfoVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        view.addSubview(alertButton)
+    }
+    
+    private lazy var alertButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 80, y: 100, width: 200, height: 50))
+        button.setTitle("Show alert", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(didTapAlert), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func didTapAlert() {
+        print("test")
     }
 }
