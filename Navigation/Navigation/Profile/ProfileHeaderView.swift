8a0b01a90bba7dc9db174506c5c2e3f6842cd4e2
Alyona Vasenina <alyona.vasenina@gmail.com>
2021-11-04 15:42:03 +0300

    Add group Profile

---
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Alena Vasenina on 04.11.2021.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+}
---
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Alena Vasenina on 04.11.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    let ProfileHV = ProfileHeaderView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        title = "Profile"
+    }
+}
