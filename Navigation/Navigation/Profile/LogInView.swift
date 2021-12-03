b654e996c63f60188d5c385b98e5d73989dd4c85
Alyona Vasenina <alyona.vasenina@gmail.com>
2021-12-03 03:38:08 +0300

    Add scrollView and keyboard

--- Navigation/Navigation/Profile/LogInView.swift
+++ Navigation/Navigation/Profile/LogInView.swift
@@ -11,13 +11,21 @@ class LogInView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubviews(logoImageView, stackView, buttonLogIn)
+        addSubview(scrollView)
+        scrollView.addSubviews(logoImageView, stackView, buttonLogIn)
         setupConstraints()
+        registerForKeyboardNotifications()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        return scroll
+    }()
+    
     
     let logoImageView: UIImageView = {
         var logo = UIImageView(image: UIImage(named: "logo"))
@@ -44,6 +52,7 @@ class LogInView: UIView {
         logIn.font = .systemFont(ofSize: 16, weight: .regular)
         logIn.tintColor = UIColor(named: "accentColor")
         logIn.autocapitalizationType = .none
+        logIn.addTarget(self, action: #selector(tapReturn(sender:)), for: .editingDidEndOnExit)
         return logIn
     }()
     
@@ -60,8 +69,13 @@ class LogInView: UIView {
         password.tintColor = UIColor(named: "accentColor")
         password.autocapitalizationType = .none
         password.isSecureTextEntry = true
+        password.addTarget(self, action: #selector(tapReturn(sender:)), for: .editingDidEndOnExit)
         return password
     }()
+    
+    @objc func tapReturn(sender: UIControl) {
+        sender.resignFirstResponder()
+    }
     
     lazy var buttonLogIn: UIButton = {
         let button = UIButton()
@@ -73,25 +87,36 @@ class LogInView: UIView {
         return button
     }()
     
+    override func layoutSubviews() {
+        scrollView.contentSize = frame.size
+        super.layoutSubviews()
+    }
     
     func setupConstraints(){
         
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            scrollView.leftAnchor.constraint(equalTo: leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: rightAnchor),
+        ])
+        
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 120),
+            logoImageView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 120),
             logoImageView.heightAnchor.constraint(equalToConstant: 100),
             logoImageView.widthAnchor.constraint(equalToConstant: 100),
-            logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
         ])
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 120),
-            stackView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
-            stackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16),
-            stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            stackView.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -16),
+            stackView.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 16),
+            stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             stackView.heightAnchor.constraint(equalToConstant: 100),
-            
         ])
         
         buttonLogIn.translatesAutoresizingMaskIntoConstraints = false
@@ -101,5 +126,36 @@ class LogInView: UIView {
             buttonLogIn.leftAnchor.constraint(equalTo: stackView.leftAnchor),
             buttonLogIn.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(onKeyboardAppear(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(onKeyboardDisappear(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: nil)
+    }
+
+    @objc func onKeyboardAppear(_ notification: NSNotification) {
+        let info = notification.userInfo!
+        let rect: CGRect = info[UIResponder.keyboardFrameBeginUserInfoKey] as! CGRect
+        
+        scrollView.contentOffset = CGPoint(x: 0, y: rect.height)
+    }
+
+    @objc func onKeyboardDisappear(_ notification: NSNotification) {
+        scrollView.contentOffset = .zero
     }
 }
