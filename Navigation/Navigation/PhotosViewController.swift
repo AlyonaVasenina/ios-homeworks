a75c10c399e3973a87d744007ebf0611631d8bdd
Alyona Vasenina <alyona.vasenina@gmail.com>
2022-01-10 23:15:10 +0300

    Add collectionView, navigationBar

--- Navigation/Navigation/PhotosViewController.swift
+++ Navigation/Navigation/PhotosViewController.swift
@@ -18,4 +18,34 @@ class PhotosViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(collectionView)
+        
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [
+            collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+        ]
+        
+            .forEach {
+                $0.isActive = true
+            }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
 }
